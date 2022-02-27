6a6e58144acbbb9b1ea95f8ea868e77ce229efae
ShuhangHou <100526410+ShuhangHou@users.noreply.github.com>
2022-02-27 18:13:09 -0500

    Update workshop1_5.py

--- workshop1_5.py
+++ workshop1_5.py
@@ -7,13 +7,14 @@ def sense(x):
 def simulate(Δt, x, u):
     x += Δt * u
     return x
-w = [0,-5.,-7.,0,0],[3,10,-5,1,1],[4,10,-3,-1,1],[9,0,-3,-0.5,1]
+# wayPoint [time, pt_x, pt_y, pVelocity_x, pvelocity_y]
+w = [0,-5.,-7.,0,0],[3,10,-6,0.5,0.5],[6,10,-3,-0.2,0.2],[9,0,-3,-0.1,0.8],[11,0,6,0.8,0],[13,3,6,0,-0.5],[15,3,1,-0.8,0],[18,-1,0,0,1],[20,0,8,1,0.5],[24,9,10,0,0],[25,9,10,0,0]
 
 k = 5
 def control(t, y):
     ux = 0.
     uy = 0.
-    for i in range(0,4):
+    for i in range(0,10):
         if w[i][0] <= t and t < w[i+1][0]:
             a0 = w[i][1]
             a1 = w[i][3]
@@ -27,7 +28,7 @@ def control(t, y):
             a3 = (2*a0+(a1+w[i+1][4])*w[i+1][0]-2*w[i+1][2])/w[i+1][0]**3
             uy = 3*a3*(t-w[i][0])**2+2*a2*(t-w[i][0])+a1+k*(a3*(t-w[i][0])**3+a2*(t-w[i][0])**2+a1*(t-w[i][0])+a0-y[1])
             return array([ux,uy])
-tf = 8.
+tf = 24.9
 Δt = 0.1    # Time step
 time = linspace(0.,tf, int(tf / Δt) + 1)  # Time interval
 
